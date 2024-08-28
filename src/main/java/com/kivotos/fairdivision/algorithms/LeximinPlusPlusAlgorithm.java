c5231f4ac6fad3c136e0b5ba2cd7f094c5ad7b64
Konstantinos Kivotos <kostasss.ryan@gmail.com>
2024-08-29 00:43:16 +0300

    Removed leximin usages and commented out the algorithm

--- src/main/java/com/kivotos/fairdivision/algorithms/LeximinPlusPlusAlgorithm.java
+++ src/main/java/com/kivotos/fairdivision/algorithms/LeximinPlusPlusAlgorithm.java
@@ -1,107 +1,105 @@
-package com.kivotos.fairdivision.algorithms;
-
-import com.kivotos.fairdivision.model.Allocation;
-import com.kivotos.fairdivision.model.FairDivisionInput;
-import com.kivotos.fairdivision.model.FairDivisionOutput;
-import com.kivotos.fairdivision.util.AgentUtility;
-import com.kivotos.fairdivision.util.ValuationChecker;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-/*
-    If true is returned then allocation A is preferred
-    If false is returned then allocation B is preferred
- */
-public class LeximinPlusPlusAlgorithm implements FairDivisionAlgorithm {
-
-    @Override
-    public FairDivisionOutput allocate(FairDivisionInput fairDivisionInput) {
-
-        if(fairDivisionInput.getLeximinFirstAllocation()==null || fairDivisionInput.getLeximinSecondAllocation()==null)
-            return new FairDivisionOutput("There was a problem with the leximin allocations provided.");
-
-
-        if (fairDivisionInput.getAgentNumber() == 2)
-            return LeximinPlusPlusTwoPlayersCase(fairDivisionInput);
-        return LeximinPlusPlus(fairDivisionInput); // Identical case
-    }
-
-    private FairDivisionOutput LeximinPlusPlus(FairDivisionInput fairDivisionInput) {
-        List<Integer> firstOrdering = calculateIncreasingOrdering(fairDivisionInput.getLeximinFirstAllocation().getAllocations(), fairDivisionInput.getValuationMatrix());
-        List<Integer> secondOrdering = calculateIncreasingOrdering(fairDivisionInput.getLeximinSecondAllocation().getAllocations(), fairDivisionInput.getValuationMatrix());
-
-        for (int l = 0; l < fairDivisionInput.getAgentNumber(); l++) {
-            int valueOfFirstAgent = ValuationChecker.getValuation(firstOrdering.get(l), fairDivisionInput.getLeximinFirstAllocation().getAllocations().get(l).getGoodsList());
-            int valueOfSecondAgent = ValuationChecker.getValuation(secondOrdering.get(l), fairDivisionInput.getLeximinSecondAllocation().getAllocations().get(l).getGoodsList());
-
-            if (valueOfFirstAgent != valueOfSecondAgent)
-                return valueOfFirstAgent < valueOfSecondAgent ? fairDivisionInput.getLeximinFirstAllocation() : fairDivisionInput.getLeximinSecondAllocation();
-
-            int sizeOfFirstAgentAllocation = fairDivisionInput.getLeximinFirstAllocation().getAllocations().get(l).getGoodsList().size();
-            int sizeOfSecondAgentAllocation = fairDivisionInput.getLeximinSecondAllocation().getAllocations().get(l).getGoodsList().size();
-
-            if (sizeOfFirstAgentAllocation != sizeOfSecondAgentAllocation)
-                return sizeOfFirstAgentAllocation < sizeOfSecondAgentAllocation ? fairDivisionInput.getLeximinFirstAllocation() : fairDivisionInput.getLeximinSecondAllocation();
-        }
-
-        return fairDivisionInput.getLeximinSecondAllocation();
-    }
-
-    private List<Integer> calculateIncreasingOrdering(List<Allocation> allocations, int[][] valuationMatrix) {
-        List<Integer> ordering = new ArrayList<>();
-        List<AgentUtility> agentUtilities = new ArrayList<>();
-
-        // Calculate utility for each agent
-        for (int i = 0; i < allocations.size(); i++) {
-            int utility = ValuationChecker.getValuation(i, allocations.get(i).getGoodsList());
-            int bundleSize = allocations.get(i).getGoodsList().size(); // Assuming allocation is a list of items
-            agentUtilities.add(new AgentUtility(i, utility, bundleSize));
-        }
-
-        // Sort agents by utility, then by bundle size if there are ties
-        Collections.sort(agentUtilities, new Comparator<AgentUtility>() {
-            @Override
-            public int compare(AgentUtility a1, AgentUtility a2) {
-                if (a1.getUtility() != a2.getUtility()) {
-                    return Integer.compare(a1.getUtility(), a2.getUtility());
-                } else {
-                    return Integer.compare(a1.getBundleSize(), a2.getBundleSize());
-                }
-            }
-        });
-
-        // Create the ordering list based on sorted utilities
-        for (AgentUtility agentUtility : agentUtilities) {
-            ordering.add(agentUtility.getAgentIndex());
-        }
-
-        return ordering;
-    }
-
-    private FairDivisionOutput LeximinPlusPlusTwoPlayersCase(FairDivisionInput fairDivisionInput) {
-
-        FairDivisionOutput leximinSolutionForPlayer1 = LeximinPlusPlus(fairDivisionInput);
-
-        Allocation bundle1 = leximinSolutionForPlayer1.getAllocations().get(0);
-        Allocation bundle2 = leximinSolutionForPlayer1.getAllocations().get(1);
-
-        int utility1ForPlayer2 = ValuationChecker.getValuation(1, bundle1.getGoodsList());
-        int utility2ForPlayer2 = ValuationChecker.getValuation(1, bundle2.getGoodsList());
-
-        // Player 2 chooses the preferred bundle
-
-        List<Allocation> finalAllocations = new ArrayList<>();
-        if (utility1ForPlayer2 >= utility2ForPlayer2) {
-            finalAllocations.add(bundle2);
-            finalAllocations.add(bundle1);
-        } else {
-            finalAllocations.add(bundle1);
-            finalAllocations.add(bundle2);
-        }
-
-        return new FairDivisionOutput (finalAllocations);
-    }
-}
+//package com.kivotos.fairdivision.algorithms;
+//
+//import com.kivotos.fairdivision.model.Allocation;
+//import com.kivotos.fairdivision.model.FairDivisionInput;
+//import com.kivotos.fairdivision.model.FairDivisionOutput;
+//import com.kivotos.fairdivision.util.AgentUtility;
+//import com.kivotos.fairdivision.util.ValuationChecker;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Comparator;
+//import java.util.List;
+//
+///*
+//    If true is returned then allocation A is preferred
+//    If false is returned then allocation B is preferred
+// */
+//public class LeximinPlusPlusAlgorithm implements FairDivisionAlgorithm {
+//
+//    @Override
+//    public FairDivisionOutput allocate(FairDivisionInput fairDivisionInput) {
+//
+//        if(fairDivisionInput.getAgentNumber()>2)
+//            return new FairDivisionOutput("There was a problem with the leximin allocations provided.");
+//
+//
+//            return LeximinPlusPlusTwoPlayersCase(fairDivisionInput);
+//    }
+//
+//    private FairDivisionOutput LeximinPlusPlus(FairDivisionInput fairDivisionInput) {
+//        List<Integer> firstOrdering = calculateIncreasingOrdering(fairDivisionInput.getLeximinFirstAllocation().getAllocations(), fairDivisionInput.getValuationMatrix());
+//        List<Integer> secondOrdering = calculateIncreasingOrdering(fairDivisionInput.getLeximinSecondAllocation().getAllocations(), fairDivisionInput.getValuationMatrix());
+//
+//        for (int l = 0; l < fairDivisionInput.getAgentNumber(); l++) {
+//            int valueOfFirstAgent = ValuationChecker.getValuation(firstOrdering.get(l), fairDivisionInput.getLeximinFirstAllocation().getAllocations().get(l).getGoodsList());
+//            int valueOfSecondAgent = ValuationChecker.getValuation(secondOrdering.get(l), fairDivisionInput.getLeximinSecondAllocation().getAllocations().get(l).getGoodsList());
+//
+//            if (valueOfFirstAgent != valueOfSecondAgent)
+//                return valueOfFirstAgent < valueOfSecondAgent ? fairDivisionInput.getLeximinFirstAllocation() : fairDivisionInput.getLeximinSecondAllocation();
+//
+//            int sizeOfFirstAgentAllocation = fairDivisionInput.getLeximinFirstAllocation().getAllocations().get(l).getGoodsList().size();
+//            int sizeOfSecondAgentAllocation = fairDivisionInput.getLeximinSecondAllocation().getAllocations().get(l).getGoodsList().size();
+//
+//            if (sizeOfFirstAgentAllocation != sizeOfSecondAgentAllocation)
+//                return sizeOfFirstAgentAllocation < sizeOfSecondAgentAllocation ? fairDivisionInput.getLeximinFirstAllocation() : fairDivisionInput.getLeximinSecondAllocation();
+//        }
+//
+//        return fairDivisionInput.getLeximinSecondAllocation();
+//    }
+//
+//    private List<Integer> calculateIncreasingOrdering(List<Allocation> allocations, int[][] valuationMatrix) {
+//        List<Integer> ordering = new ArrayList<>();
+//        List<AgentUtility> agentUtilities = new ArrayList<>();
+//
+//        // Calculate utility for each agent
+//        for (int i = 0; i < allocations.size(); i++) {
+//            int utility = ValuationChecker.getValuation(i, allocations.get(i).getGoodsList());
+//            int bundleSize = allocations.get(i).getGoodsList().size(); // Assuming allocation is a list of items
+//            agentUtilities.add(new AgentUtility(i, utility, bundleSize));
+//        }
+//
+//        // Sort agents by utility, then by bundle size if there are ties
+//        Collections.sort(agentUtilities, new Comparator<AgentUtility>() {
+//            @Override
+//            public int compare(AgentUtility a1, AgentUtility a2) {
+//                if (a1.getUtility() != a2.getUtility()) {
+//                    return Integer.compare(a1.getUtility(), a2.getUtility());
+//                } else {
+//                    return Integer.compare(a1.getBundleSize(), a2.getBundleSize());
+//                }
+//            }
+//        });
+//
+//        // Create the ordering list based on sorted utilities
+//        for (AgentUtility agentUtility : agentUtilities) {
+//            ordering.add(agentUtility.getAgentIndex());
+//        }
+//
+//        return ordering;
+//    }
+//
+//    private FairDivisionOutput LeximinPlusPlusTwoPlayersCase(FairDivisionInput fairDivisionInput) {
+//
+//        FairDivisionOutput leximinSolutionForPlayer1 = LeximinPlusPlus(fairDivisionInput);
+//
+//        Allocation bundle1 = leximinSolutionForPlayer1.getAllocations().get(0);
+//        Allocation bundle2 = leximinSolutionForPlayer1.getAllocations().get(1);
+//
+//        int utility1ForPlayer2 = ValuationChecker.getValuation(1, bundle1.getGoodsList());
+//        int utility2ForPlayer2 = ValuationChecker.getValuation(1, bundle2.getGoodsList());
+//
+//        // Player 2 chooses the preferred bundle
+//
+//        List<Allocation> finalAllocations = new ArrayList<>();
+//        if (utility1ForPlayer2 >= utility2ForPlayer2) {
+//            finalAllocations.add(bundle2);
+//            finalAllocations.add(bundle1);
+//        } else {
+//            finalAllocations.add(bundle1);
+//            finalAllocations.add(bundle2);
+//        }
+//
+//        return new FairDivisionOutput (finalAllocations);
+//    }
+//}
--- src/main/java/com/kivotos/fairdivision/mapper/FairDivisionInputMapper.java
+++ src/main/java/com/kivotos/fairdivision/mapper/FairDivisionInputMapper.java
@@ -1,17 +1,10 @@
 package com.kivotos.fairdivision.mapper;
 
 import com.kivotos.fairdivision.dto.WebsiteInputDTO;
-import com.kivotos.fairdivision.model.Allocation;
 import com.kivotos.fairdivision.model.FairDivisionInput;
-import com.kivotos.fairdivision.model.FairDivisionOutput;
 import com.kivotos.fairdivision.util.ValuationChecker;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface FairDivisionInputMapper {
@@ -21,8 +14,6 @@ public interface FairDivisionInputMapper {
     @Mapping(target = "valuationType", source = "valuationDropdownValue")
     @Mapping(target = "algorithmId", source = "algorithmDropdownValue")
     @Mapping(target = "valuationMatrix", expression = "java(convertValuations(dto.getValuationContainer(), dto.getAgentSliderValue(), dto.getGoodsSliderValue()))")
-    @Mapping(target = "leximinFirstAllocation", expression = "java(convertLeximinAllocations(dto.getLeximinFirstAllocation(),dto.getGoodsSliderValue()))")
-    @Mapping(target = "leximinSecondAllocation", expression = "java(convertLeximinAllocations(dto.getLeximinSecondAllocation(),dto.getGoodsSliderValue()))")
     @Mapping(target = "identicalValuation", expression = "java(isValuationIdentical())")
     FairDivisionInput toFairDivisionInput(WebsiteInputDTO dto);
 
@@ -40,45 +31,6 @@ public interface FairDivisionInputMapper {
 
         return matrix;
     }
-
-    default FairDivisionOutput convertLeximinAllocations(int[][] leximinAllocation, int numberOfGoods) {
-        List<Allocation> allocationsList = new ArrayList<>();
-        Set<Integer> uniqueItems = new HashSet<>();
-
-        // Check if the total number of items matches the number of goods
-        int totalItems = 0;
-        for (int i = 0; i < leximinAllocation.length; i++) {
-            totalItems += leximinAllocation[i].length;
-            for (int j = 0; j < leximinAllocation[i].length; j++) {
-                if (leximinAllocation[i][j] < 0 || leximinAllocation[i][j] >= numberOfGoods) {
-                    return null;
-                }
-                uniqueItems.add(leximinAllocation[i][j]);
-            }
-        }
-
-        // Ensure the number of unique items equals the number of goods
-        if (uniqueItems.size() != numberOfGoods) {
-            return null;
-        }
-
-        // Ensure the total number of items equals the number of goods
-        if (totalItems != numberOfGoods) {
-            return null;
-        }
-
-        // Create allocations
-        for (int i = 0; i < leximinAllocation.length; i++) {
-            Allocation allocation = new Allocation(i);
-            for (int j = 0; j < leximinAllocation[i].length; j++) {
-                allocation.add(leximinAllocation[i][j], ValuationChecker.getValuationMatrix());
-            }
-            allocationsList.add(allocation);
-        }
-
-        return new FairDivisionOutput(allocationsList);
-    }
-
 
     default boolean isValuationIdentical() {
         boolean isIdentical = true;
