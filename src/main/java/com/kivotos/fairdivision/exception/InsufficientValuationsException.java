657f080f2d847c9b68cbfd795869f834025df9d9
Konstantinos Kivotos <kostasss.ryan@gmail.com>
2024-08-29 18:23:52 +0300

    Deleted unused classes

--- src/main/java/com/kivotos/fairdivision/exception/InsufficientValuationsException.java
+++
@@ -1,8 +0,0 @@
-package com.kivotos.fairdivision.exception;
-
-public class InsufficientValuationsException extends Exception {
-    public InsufficientValuationsException(String message) {
-        super(message);
-    }
-}
-
